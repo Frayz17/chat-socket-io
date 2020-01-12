af9713180959231df7ec8eae53d988663f0879bd
Andrey Dmitriev <andrydmit@gmail.com>
2020-01-12 22:14:35 +0200

    initial

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Join from 'components/Join';
+import Chat from 'components/Chat';
+
+export default function App() {
+  return (
+    <>
+      <Route exact path='/'>
+        <Join />
+      </Route>
+      <Route exact path='/'>
+        <Chat />
+      </Route>
+    </>
+  );
+}
