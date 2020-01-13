7d0e682ec754219da2e23e5045986ac6051b124a
Andrey Dmitriev <andrydmit@gmail.com>
2020-01-13 23:22:37 +0200

    join component creating

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,22 @@ import { Route } from 'react-router-dom';
 import Join from 'components/Join';
 import Chat from 'components/Chat';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import 'typeface-roboto';
+
 export default function App() {
   return (
     <>
-      <Route exact path='/'>
-        <Join />
-      </Route>
-      <Route exact path='/chat'>
-        <Chat />
-      </Route>
+      <CssBaseline />
+      <Container>
+        <Route exact path='/'>
+          <Join />
+        </Route>
+        <Route exact path='/chat'>
+          <Chat />
+        </Route>
+      </Container>
     </>
   );
 }
--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,9 +1,60 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { Link as RouterLink } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: '15vh',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  paper: {
+    width: 350,
+    minHeight: 300
+  },
+  box: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    minHeight: 300
+  }
+}));
 
 export default function Join() {
   const [name, setName] = React.useState('');
   const [room, setRoom] = React.useState('');
 
-  return <div>Join</div>;
+  const classes = useStyles();
+
+  const checkNameRoomFilled = (e) => {
+    return !name || !room ? e.preventDefault() : null;
+  };
+
+  return (
+    <Grid container className={classes.root}>
+      <Paper className={classes.paper}>
+        <Box className={classes.box}>
+          <Typography variant={'h4'}>Join</Typography>
+          <TextField label='Name' variant='outlined' />
+          <TextField label='Room' variant='outlined' />
+          <Button
+            color='primary'
+            component={RouterLink}
+            onClick={checkNameRoomFilled}
+            to={`/chat?name=${name}&room=${room}`}
+          >
+            Sign In
+          </Button>
+        </Box>
+      </Paper>
+    </Grid>
+  );
 }
