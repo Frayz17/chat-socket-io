8ed6e5a93f873d367a52cbdd3af484abc4c65850
Andrey <andrydmit@gmail.com>
2020-01-13 16:54:17 +0200

    update

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Join() {
+  const [name, setName] = React.useState('');
+  const [room, setRoom] = React.useState('');
+
   return <div>Join</div>;
 }
--- server/index.js
+++ server/index.js
@@ -4,4 +4,16 @@ const io = require('socket.io').apply(server);
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+
+io.on('connection', (socket) => {
+  console.log('a user connected!!!!');
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected');
+  });
+});
+
+app.use(router);
+
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
