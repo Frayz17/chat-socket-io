b38f7ce0ca49ba7636c6d5c22d40b175042d82e4
Andrey Dmitriev <andrydmit@gmail.com>
2020-01-16 16:37:00 +0200

    created users methods

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
 const PORT = process.env.PORT || 5000;
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
 
 const router = require('./router');
 
 io.on('connection', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room);
+    console.log(`${name} ${room} is connected`);
   });
 
   socket.on('disconnect', () => {
